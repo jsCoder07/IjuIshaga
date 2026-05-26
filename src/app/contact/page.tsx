7061fbf656210c2256321207412df6363a4813cb
Jeremiah Adeboye <91225691+jsCoder07@users.noreply.github.com>
2026-05-26 10:59:26 +0100

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -34,9 +34,9 @@ export default function ContactPage() {
           </form>
 
           <div className="mt-12 text-center text-gray-700">
-            <p><GiRotaryPhone /> +234 703 377 7772</p>
-            <p><MdEmail /> reservations@ijuishagacitylodge.com.ng</p>
-            <p><ImLocation2 /> 221 Iju-Ishaga Road (Gudugba Bus Stop) Ifako-Ijaiye, Lagos</p>
+            <p className="flex"><GiRotaryPhone /> +234 703 377 7772</p>
+            <p className="flex"><MdEmail /> reservations@ijuishagacitylodge.com.ng</p>
+            <p className="flex"><ImLocation2 /> 221 Iju-Ishaga Road (Gudugba Bus Stop) Ifako-Ijaiye, Lagos</p>
           </div>
         </div>
       </section>
